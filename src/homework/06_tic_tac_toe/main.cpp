e74e11d0f217761a4d431008ddf7ce07650caf61
Ngoc031028 <ngochtt602@gmail.com>
2022-07-01 20:00:40 -0500

    Homework 5

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,33 @@
-
+#include<iostream>
+#include<string>
+#include "tic_tac_toe.h"
+using namespace std;
 int main() 
 {
+	TicTacToe game;
+	string first_player{" "};
+	char user_choice;
 
+	do
+	{
+		cout << "Enter first player: ";
+		cin >> first_player;
+		game.start_game(first_player);
+		
+		int position;
+
+		while(!game.game_over())
+		{
+			cout << "Enter position: ";
+			cin >> position;
+			game.mark_board(position);
+			game.display_board();
+		}
+
+		cout << "Play again? (y/n): ";
+		cin >> user_choice;
+
+	} while (user_choice == 'Y' || user_choice == 'y');
+	
 	return 0;
 }
